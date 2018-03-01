61c6e59e803ec5f8de469066f2430e622ef47e1a
Simisola Olofinboba <simisol@princeton.edu>
2018-02-28 20:18:13 -0500

    Project creation and addition of the database

---
+++ app/src/main/java/com/example/simiboba/govcom/SubscribershipMenu.java
@@ -0,0 +1,13 @@
+package com.example.simiboba.govcom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubscribershipMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscribership_menu);
+    }
+}
